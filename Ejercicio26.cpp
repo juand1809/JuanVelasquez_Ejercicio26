b1d0e7236b26386fea0debce8785ee30b052eb83
juand1809 <jd.velasquezf@uniandes.edu.co>
2019-04-23 12:51:42 +0000

    inicial

--- Ejercicio26.cpp
+++ Ejercicio26.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 
 void solve_equation_euler(float t_init, float t_end, float delta_t, float omega, string filename);
-void solve_equation_implicit(float t_init, float t_end, float delta_t, float omega, string filename);
+
 int main(){
   float omega=0.1;
   float omega=0.1;
